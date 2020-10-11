0013eb80e272f760bf8b568352b90d125742aadd
adel <shadihh9@gmail.com>
2020-10-11 19:58:32 +0300

    Add files via upload

---
+++ nucleir.sh
@@ -0,0 +1,20 @@
+nucleir() {
+	local TPL="$HOME/nuclei-templates"
+
+	[[ -z "${1}" ]] && { echo "-target/-l ?"; return; }
+	[[ -z "${2}" ]] && { echo "Input target?"; return; }
+
+	local T=""
+
+	for i in `ls -1d ${TPL}/*/`; do
+		if [[ ! "${i}" =~ (brute-force|examples|payloads) ]]; then
+			T+="-t ${i} "
+		fi
+	done
+
+	eval "nuclei -c 200 ${1} ${2} ${T} \
+		-exclude ${TPL}/technologies/tech-detect.yaml \
+		-o \"$(basename $2)_nuclei-results.txt\""
+}
+
+nucleir $1 $2
